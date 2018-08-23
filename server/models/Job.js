c53722aaa307d5041541e7149cf4946374b976af
Jarrod <Demonico@users.noreply.github.com>
2018-08-23 16:58:23 -0600

    modified job model and added job page method to scraper

--- server/models/Job.js
+++ server/models/Job.js
@@ -4,11 +4,13 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const JobSchema = new Schema({
+  company: { type: String },
+  hq: { type: String },
   title: { type: String },
   description: { type: String },
-  guid: { type: String },
+  apply: { type: String },
   link: { type: String },
-  pubDate: { type: String }
+  // pubDate: { type: String }
 })
 
 const Job = mongoose.model('Job', JobSchema)
--- server/scraper.js
+++ server/scraper.js
@@ -10,7 +10,7 @@ mongoose.connect(
 )
 
 // Require all models
-const db = require('./models')
+// const db = require('./models')
 
 const scraper = {
   // Scrape Job List Page
@@ -26,21 +26,45 @@ const scraper = {
         })
       })
       .catch(err => console.log(err))
-  }
+  },
 
   // Scrape Job Description Page
-  // ###################################
-  // Grab:
-  // -- Title
-  // -- Company Name
-  // -- HQ (if available)
-  // -- Description (keep html for now)
-  // -- How to apply
+  wwrJobPage(url) {
+    // Grab:
+
+    // -- Description (keep html for now)
+    // -- How to apply
+    axios
+      .get(url)
+      .then(res => {
+        let job = {}
+        const $ = cheerio.load(res.data)
+        let header = $('.listing-header-container')
+        // -- Title
+        job.title = header.children('h1').text()
+        // -- Company Name
+        job.company =  header.children('h2').children('.company').text()
+        // -- HQ (if available)
+        job.hq =  header.children('h2').children('.location').text()
+
+        
+        let desc = $('.listing-container')
+        job.description = desc.html()
+
+        let apply = $('.apply>p')
+        job.apply = apply.html()
+
+        console.log(job)
+      })
+      .catch(err => console.log(err))
+  }
+
   // ###################################
   // add to job model
 }
 
-module.exports = scraper
+scraper.wwrJobPage('https://weworkremotely.com/remote-jobs/pixelcabin-junior-front-end-developer-1')
+// module.exports = scraper
 
 // scrapeRSS('https://weworkremotely.com/categories/remote-programming-jobs.rss')
 // scraper.wwrDevJobList('https://weworkremotely.com/categories/remote-programming-jobs')
